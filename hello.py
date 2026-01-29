49a1b09f1b6fbabab6f1a85f3f1e357594511a7d
Nischal Shrestha <sthanischal5060@gmail.com>
2026-01-29 22:32:15 +0545

    strong password generator

--- hello.py
+++ hello.py
@@ -1,7 +1,17 @@
 
-name=input("Enter name")
-name=name.lower()
-name=name.strip()
-name=name.split()
-name="_".join(name)
-print(name)
+# name=input("Enter name")
+# name=name.lower()
+# name=name.strip()
+# name=name.split()
+# name="_".join(name)
+# print(name)
+
+
+# password=input("Enter password: ")
+# # password="Passion"
+# password=password.lower()
+# clean={"a":"@","e":"3","i":"!","o":"0","s":"$"}
+# translator=str.maketrans(clean)
+# strong_passord=password.translate(translator)
+# strong_passord=strong_passord+"0##9"
+# print(strong_passord)
