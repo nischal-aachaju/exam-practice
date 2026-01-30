ee701968cb83873fe4301ddfdb28e23b0eb5216a
Nischal Shrestha <sthanischal5060@gmail.com>
2026-01-30 20:22:22 +0545

    iis qualified?

--- practice_questions.py
+++ practice_questions.py
@@ -32,10 +32,34 @@
 # print(lst)
 
 
-sample_list = [87, 45, 41, 65, 94, 41, 99, 94]
-tuple_list=tuple(set(sample_list))
-max_num=max(tuple_list)
-min_num=min(tuple_list)
-print("max num:",max_num)
-print("min num:",min_num)
-print(tuple_list)
+# sample_list = [87, 45, 41, 65, 94, 41, 99, 94]
+# tuple_list=tuple(set(sample_list))
+# max_num=max(tuple_list)
+# min_num=min(tuple_list)
+# print("max num:",max_num)
+# print("min num:",min_num)
+# print(tuple_list)
+
+applicant = { "name": "Priya", 
+             "skills":["Java", "SQL"],
+             "experience_years": 1
+             } 
+
+required_skills = {"Python", "Java"} 
+
+# for skill in applicant["skills"]:
+#     if skill in required_skills:
+#         if applicant["experience_years"]>= 2:
+#             print(f"{applicant["name"]} is qualifued")
+#         else:
+#             print(f"{applicant["name"]} is not qualfyed")
+
+
+has_requierd_skill=len(set(applicant["skills"]).intersection(required_skills))>0
+has_experiences=applicant["experience_years"]>=2
+
+if has_experiences and has_requierd_skill:
+    print(f"{applicant['name']} is qualidied")
+
+else:
+    print(f"{applicant['name']} is no qualidied")
