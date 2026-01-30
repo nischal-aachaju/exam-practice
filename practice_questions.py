10add8c4eeb40f60fd71273e39fee15d222335d2
Nischal Shrestha <sthanischal5060@gmail.com>
2026-01-30 11:17:21 +0545

    practicing set,dict,list,tuple

---
+++ practice_questions.py
@@ -0,0 +1,17 @@
+# Items = [3,5,7,9,11,13]
+
+# idx4=Items.pop(3)
+# print(Items,idx4)
+# Items.insert(1,idx4)
+# print(Items)
+# Items.append(idx4)
+# print(Items)
+
+# first_set = {23, 42, 65, 57, 78, 83, 29} 
+# second_set = {57,83, 29, 67, 73, 43, 48}
+
+# intersec=first_set.intersection(second_set)
+# print(intersec)
+
+# first_set=first_set-intersec
+# print(first_set)
