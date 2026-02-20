99579e1460d20a02c73d549fd084f49370e778cc
Nischal Shrestha <sthanischal5060@gmail.com>
2026-02-20 14:10:56 +0545

    week 3 practice

--- final_exam.py
+++ final_exam.py
@@ -1,28 +1,67 @@
-# a=list(range(20,10,-2))
-# print(a)
+# # a=list(range(20,10,-2))
+# # print(a)
 
-# a=2
-# b=4
-# b/=a
-# print(b)
+# # a=2
+# # b=4
+# # b/=a
+# # print(b)
 
-name=" Laxmi deptoka "
-name=name.lower().strip()
-name=name.split()
-user_name="_".join(name)
+# name=" Laxmi deptoka "
+# name=name.lower().strip()
+# name=name.split()
+# user_name="_".join(name)
 
-print(user_name)
+# print(user_name)
 
-password="passion"
-password=password.lower()
-le={"a":"","s":""}
-translator=str.maketrans("aeios","@3!0$")
-# translator=str.maketrans(le)
-secret_password=password.translate(translator)+"0##9"
-print(secret_password)
+# password="passion"
+# password=password.lower()
+# le={"a":"","s":""}
+# translator=str.maketrans("aeios","@3!0$")
+# # translator=str.maketrans(le)
+# secret_password=password.translate(translator)+"0##9"
+# print(secret_password)
 
-word="I love south indian movie"
-word_list=word.title().split()
-word_list_rev=word_list[::-1]
-word_rev=" ".join(word_list_rev)
-print(word_rev)
+# word="I love south indian movie"
+# word_list=word.title().split()
+# word_list_rev=word_list[::-1]
+# word_rev=" ".join(word_list_rev)
+# print(word_rev)
+
+# # num=int(input("Enter number: "))
+# num=1
+# if num %2==0: 
+#     print("then number is even")
+# else :
+#     print("The number is odd")
+
+# # char=input("Enter char:")
+# char="a"
+# char=char[:1].lower()
+# print(char)
+# vowel=["a","e","i","o","u"]
+# if char in vowel:
+#     print("Vowel")
+# else:
+#     print("Consonent")
+    
+    
+# months = {
+#     1: "January",
+#     2: "February",
+#     3: "March",
+#     4: "April",
+#     5: "May",
+#     6: "June",
+#     7: "July",
+#     8: "August",
+#     9: "September",
+#     10: "October",
+#     11: "November",
+#     12: "December"
+# }
+
+# num=int(input("Enter number betn 1 to 12: "))
+# if num >0 and num <=12:
+#     print(f"Month: {months[num]}")
+# else:
+#     print("Invalid number")
